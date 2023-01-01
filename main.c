afe7e9873ae81fa3bd6ea09fdec3674c4b11717d
student <student@codeby.local>
2023-01-01 00:02:00 +0000

    add static content support

--- main.c
+++ main.c
@@ -7,6 +7,18 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
     return;
   }
 
+  struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+
+  if (mg_http_match_uri(hm, "/static/#")) {
+    struct mg_http_serve_opts opts;
+    memset(&opts, 0, sizeof(opts));
+
+    opts.root_dir = ".";
+
+    mg_http_serve_dir(c, hm, &opts);
+    return;
+  }
+
   mg_http_reply(c, 200, "", "%s\n", "It works!");
 }
 
